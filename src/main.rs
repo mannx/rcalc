3c71bcd5331411d7b9e62aafc3dd60520a1b7034
Mannx <18175956+mannx@users.noreply.github.com>
2022-11-21 19:18:22 -0400

    initial working start

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,152 @@
+#![allow(non_snake_case)]
 use std::io;
 use std::io::Write;
 
-use nom::{
-    branch::alt,
-    character::complete::{char, one_of},
-    combinator::{opt, recognize},
-    multi::{many0, many1},
-    sequence::{preceded, terminated, tuple},
-    IResult,
+use phf::phf_map;
+
+#[derive(PartialEq, Clone)]
+enum Operation {
+    Value(f64), // value to push to stack
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Sum, // Sum the entire stack togther
+    Clear,
+}
+
+static OPERATION_PARSE: phf::Map<&'static str, Operation> = phf_map! {
+    "+" => Operation::Add,
+    "-" => Operation::Sub,
+    "*" => Operation::Mul,
+    "/" => Operation::Div,
+    "s" => Operation::Sum,
+    "c" => Operation::Clear,
 };
 
-#[derive(Debug, PartialEq)]
-pub struct Value {
-    pub val: f64,
+fn get_val(input: &str) -> Option<f64> {
+    let float = input.parse::<f64>();
+    match float {
+        Ok(f) => return Some(f),
+        Err(_) => (),
+    }
+
+    let dec = input.parse::<i32>();
+    match dec {
+        Ok(i) => return Some(f64::from(i)),
+        Err(_) => (),
+    }
+
+    return None;
 }
 
-fn float(input: &str) -> IResult<&str, &str> {
-    alt((
-        // Case one: .42
-        recognize(tuple((
-            char('.'),
-            decimal,
-            opt(tuple((one_of("eE"), opt(one_of("+-")), decimal))),
-        ))), // Case two: 42e42 and 42.42e42
-        recognize(tuple((
-            decimal,
-            opt(preceded(char('.'), decimal)),
-            one_of("eE"),
-            opt(one_of("+-")),
-            decimal,
-        ))), // Case three: 42. and 42.42
-        recognize(tuple((decimal, char('.'), opt(decimal)))),
-    ))(input)
+// checks if this is operation type (anything not a value)
+fn get_op(input: &str) -> Option<Operation> {
+    let op = OPERATION_PARSE.get(input).cloned();
+    return match op {
+        Some(op) => Some(op),
+        None => None,
+    };
 }
 
-fn decimal(input: &str) -> IResult<&str, &str> {
-    recognize(many1(terminated(one_of("0123456789"), many0(char('_')))))(input)
+// parse the input and extract the operation if any
+// returns None if unable to parse
+fn parse_input(input: &str) -> Option<Operation> {
+    match get_op(&input) {
+        Some(o) => return Some(o),
+        None => (),
+    }
+
+    match get_val(&input) {
+        Some(i) => return Some(Operation::Value(i)),
+        None => (),
+    }
+
+    return None;
 }
 
-fn get_dec(input: &str) -> Option<&str> {
-    let res = decimal(&input);
+fn display_stack(stack: &Vec<f64>) {
+    let mut lock = io::stdout().lock();
+    for i in stack {
+        write!(lock, "{} ", i).expect("Unable to write to stdout!");
+    }
 
-    return match res {
-        Ok((_, o)) => Some(o),
+    print!("\n\r");
+    io::stdout().flush().expect("Unable to flush stdout!");
+}
+
+fn add_op(rhs: f64, lhs: f64) -> f64 {
+    rhs + lhs
+}
+
+fn sub_op(rhs: f64, lhs: f64) -> f64 {
+    rhs - lhs
+}
+
+fn mul_op(rhs: f64, lhs: f64) -> f64 {
+    rhs * lhs
+}
+
+fn div_op(rhs: f64, lhs: f64) -> f64 {
+    lhs / rhs
+}
+
+fn math_func(stack: &mut Vec<f64>, f: fn(f64, f64) -> f64) -> Option<f64> {
+    let a = match stack.pop() {
+        Some(n) => n,
+        None => return None,
+    };
+
+    let b = match stack.pop() {
+        Some(n) => n,
+        None => return None,
+    };
+
+    return Some(f(a, b));
+}
+
+fn sum_op(stack: &mut Vec<f64>) -> Option<f64> {
+    let total = stack.iter().sum();
+    stack.clear();
+    return Some(total);
+}
+
+fn do_op(stack: &mut Vec<f64>, op: &Operation) -> Result<bool, &'static str> {
+    // match block for functions that don't return any data
+    // if we return true, skip the rest of the function
+    let ret = match op {
+        Operation::Clear => {
+            stack.clear();
+            true
+        }
+        _ => false,
+    };
+
+    if ret == true {
+        return Ok(true);
+    }
+
+    let res = match op {
+        Operation::Add => math_func(stack, add_op),
+        Operation::Sub => math_func(stack, sub_op),
+        Operation::Mul => math_func(stack, mul_op),
+        Operation::Div => math_func(stack, div_op),
+        Operation::Sum => sum_op(stack),
         _ => None,
     };
-}
 
-fn get_float(input: &str) -> Option<&str> {
-    let res = float(&input);
-
-    return match res {
-        Ok((_, o)) => Some(o),
-        _ => None,
-    };
-}
-
-fn get_val(input: &str) -> Option<Value> {
-    let val = match get_float(&input) {
-        Some(s) => s.parse::<f64>().unwrap(),
-        None => match get_dec(&input) {
-            Some(s) => s.parse::<f64>().unwrap(),
-            None => f64::NAN,
-        },
-    };
-
-    if val == f64::NAN {
-        return None;
-    } else {
-        return Some(Value { val });
+    match res {
+        Some(n) => {
+            stack.push(n);
+            return Ok(true); // retunr the new value
+        }
+        None => return Err("Unable to perform operation"),
     }
 }
 
-// fn to_val(input: &str) -> IResult<&str, Value> {
-//     // check for floating value first (requires . in)
-//     let res = float(&input);
-//     let mut val = Value { val: 0. };
-// }
-
 fn main() {
+    let mut stack: Vec<f64> = Vec::new();
+
     loop {
         print!(">");
         io::stdout().flush().unwrap();
@@ -89,23 +157,31 @@ fn main() {
             .expect("unable to read input");
 
         // trim the string of any excess whitespace first
+        // and split into individual portions
         input = input.trim().to_string();
-        println!(" [input: ({})]", input);
+        let inpVec = input.split_whitespace().collect::<Vec<&str>>();
 
-        let res = get_val(&input);
-        match res {
-            Some(n) => println!(">{}", n.val),
-            None => println!("> [Not a decimal]"),
+        for inp in inpVec {
+            let val = parse_input(&inp);
+            match val {
+                None => {
+                    println!("< --INVALID INPUT --");
+                    continue;
+                }
+                Some(op) => match op {
+                    Operation::Value(v) => {
+                        stack.push(v);
+                    }
+                    op => {
+                        match do_op(&mut stack, &op) {
+                            Ok(_) => (),
+                            Err(e) => println!("  -- Error: {}", e),
+                        }
+                        ()
+                    }
+                },
+            }
         }
-
-        // let res = float(&input);
-
-        // match res {
-        //     Ok((a, b)) => {
-        //         println!("< (a) = {}", a);
-        //         println!("< (b) = {}", b);
-        //     }
-        //     Err(e) => println!("Unable to parse: {}", e),
-        // }
+        display_stack(&stack);
     }
 }
